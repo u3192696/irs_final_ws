eb0cd9037659d154c386b95ad1c43fc621816bda
u3192696 <u3192696@uni.canberra.edu.au>
2025-10-17 09:11:43 +0000

    Initial commit for IRS final workspace

---
+++ src/hs_robot_system/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+package_name = 'hs_robot_system'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(exclude=['test'], include=['hs_robot_system', 'hs_robot_system.*']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Daniel',
+    maintainer_email='u3192696@uni.canberra.edu.au',
+    description='Final integration project for IRS',
+    license='MIT',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'nav_controller = hs_robot_system.nodes.nav_controller:main',
+            'arm_manager = hs_robot_system.nodes.arm_manager:main',
+            'plc_location_service = hs_robot_system.nodes.plc_location_service:main',
+            'robot_state_service = hs_robot_system.nodes.robot_state_service:main',
+        ],
+    },
+)
